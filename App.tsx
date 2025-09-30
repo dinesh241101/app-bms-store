c7e9199c1c94dde9e5d9c8b7178a967f1899f967
dinesh-7228 <dinesh@countrydelight.in>
2025-09-30 15:08:16 +0530

    new project

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './src/navigation/AppNavigator';
+import { CartProvider } from './src/context/CartContext';
+
+const App = () => {
+  return (
+    <CartProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </CartProvider>
+  );
+};
+
+export default App;
